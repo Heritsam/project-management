12f2ca9ce0f3154faa62e0f8b61f3d3f2e40cdac
Ariq Heritsa <ariqhm@gmail.com>
2019-12-09 17:24:54 +0700

    Add chart library

--- resources/views/project/show.blade.php
+++ resources/views/project/show.blade.php
@@ -4,6 +4,86 @@
     @include('layouts.headers.title', ['title' => 'Project Overview', 'subtitle' => $project->name])
 
     <div class="container-fluid mt--7">
+        <div class="row justify-content-center">
+            <div class="col-md-4 mb-4">
+                <div class="card card-stats shadow border-0 mb-4 mb-lg-0">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <h5 class="card-title text-uppercase text-muted mb-0">Overall</h5>
+                                <span class="h2 font-weight-bold mb-0">
+                                    {{ $project->done_percentage() }}
+                                </span>
+                            </div>
+                            <div class="col-auto">
+                                <div class="icon icon-shape bg-gradient-primary text-white rounded-circle shadow">
+                                    <i class="fa fa-percentage"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <p class="mt-3 mb-0 text-muted text-sm">
+                            <span class="text-nowrap">Out of</span>
+                            <span class="text-success mr-2">
+                                {{ $project->timelines->count() }} Timelines
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-md-4 mb-4">
+                <div class="card card-stats shadow border-0 mb-4 mb-lg-0">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <h5 class="card-title text-uppercase text-muted mb-0">Timelines</h5>
+                                <span class="h2 font-weight-bold mb-0">
+                                    {{ $project->timelines->count() . Str::plural(" Timeline", $project->timelines->count()) }}
+                                </span>
+                            </div>
+                            <div class="col-auto">
+                                <div class="icon icon-shape bg-gradient-danger text-white rounded-circle shadow">
+                                    <i class="fa fa-project-diagram"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <p class="mt-3 mb-0 text-muted text-sm">
+                            <span class="text-success mr-2">
+                                {{ $project->timelines->where('date_done', '!=', null)->count() . "/" . $project->timelines->count() }}
+                            </span>
+                            <span class="text-nowrap">Timelines done</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 mb-4">
+                <div class="card card-stats shadow border-0 mb-4 mb-lg-0">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <h5 class="card-title text-uppercase text-muted mb-0">Contributors</h5>
+                                <span class="h2 font-weight-bold mb-0">
+                                    {{ $project->contributors->count() . Str::plural(" Contributor", $project->contributors->count()) }}
+                                </span>
+                            </div>
+                            <div class="col-auto">
+                                <div class="icon icon-shape bg-gradient-success text-white rounded-circle shadow">
+                                    <i class="fa fa-users"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <p class="mt-3 mb-0 text-muted text-sm">
+                            <span class="text-nowrap">Started by</span>
+                            <span class="text-success mr-2">
+                                {{ $project->started_by->name }}
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
         <div class="card shadow">
             <div class="card-body">
 
@@ -53,13 +133,15 @@
                 @csrf
                 @method('DELETE')
 
-                @if (in_array(Auth::user()->id, $project->contributors->pluck('user_id')->toArray()))
-                    <a href="{{ route('project.edit', $project->id) }}" class="btn btn-link btn-icon">
-                        <i class="fa fa-pen"></i> Edit Project
-                    </a>
-                @endif
                 
                 @if ($project->started_by->id == Auth::user()->id)
+
+                    {{-- @if (in_array(Auth::user()->id, $project->contributors->pluck('user_id')->toArray())) --}}
+                        <a href="{{ route('project.edit', $project->id) }}" class="btn btn-link btn-icon">
+                            <i class="fa fa-pen"></i> Edit Project
+                        </a>
+                    {{-- @endif --}}
+                    
                     <button type="submit" class="btn btn-link text-danger btn-icon"
                         onclick="return confirm('Are you sure you want to delete this project? this action cannot be reverted')">
                         <i class="fa fa-trash"></i> Delete Project
